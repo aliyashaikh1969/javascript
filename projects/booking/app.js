a1f5c4bca11dba67320ce10e131b653f78694863
aliyashaikh1969 <aliyashaikh1969@gmail.com>
2025-01-11 00:07:05 +0530

    seats selecting

--- projects/booking/app.js
+++ projects/booking/app.js
@@ -6,22 +6,41 @@ const rows = document.querySelectorAll(".seats .row")
 
 let rowNumber = 0
 
-const selectingRows=()=>{
 
-    const columns = rows[rowNumber].children
-   
-    console.log("columns",columns)
-    
-}
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    if (rowNumber < 10) {
-        console.log(rowNumber)
-        selectingRows()
+
+const selectingRows =()=>{
+    let value = input.value;
+    if(value<=5){
+        bookingStatus(value,rowNumber)
+        rowNumber++
+    }else if(value>5){
+        let newValue = 5
+        bookingStatus(newValue,rowNumber)
         rowNumber++
 
-    } else {
-        console.log("end")
+        let addingValue = value - newValue
+        bookingStatus(addingValue,rowNumber)
+
+        rowNumber++
+    }
+    input.value = ""
+}
+
+const bookingStatus = (invalue,numberOfRow)=>{
+    console.log(invalue,numberOfRow)
+
+
+    for(let i = 0 ;i<invalue;i++){
+        rows[numberOfRow].children[i].classList.add("booked");
+    }
+
+}
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    if(rowNumber<10){
+        selectingRows()
     }
 
 })
