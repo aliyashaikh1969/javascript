9bac44b743a5344f9f8b4387e85fbfbde8aa0577
aliyashaikh1969 <aliyashaikh1969@gmail.com>
2026-04-23 23:11:13 +0530

    coupon applied successfully

--- projects/add-to-cart/app.js
+++ projects/add-to-cart/app.js
@@ -8,6 +8,16 @@ const productList = document.querySelector(".product-list");
 const cartCount = document.querySelector(".cart-count")
 
 const productDetails = document.querySelector(".product-details table tbody")
+const emptyContainer = document.querySelector(".cart-page .container")
+const subTotal = document.querySelector(".s-total");
+const discount = document.querySelector(".discount");
+const delivery = document.querySelector(".delivery");
+const total = document.querySelector(".total");
+const availabeOffers = document.querySelector(".available-offers .offer-list")
+const appliedOfferList = document.querySelector(".applied-offer-list");
+const discountText = document.querySelector('.dis-text');
+const couponInput = document.querySelector(".discount-form input")
+const applyBtn = document.querySelector(".discount-form .btn")
 
 updateCartCount()
 
@@ -47,6 +57,7 @@ function addProducts(data) {
             productList.appendChild(card)
             card.querySelector(".cart-card").addEventListener("click", () => {
                 addToCart(item);
+                showToast("product added" ,"success")
             });
         });
     }
@@ -63,17 +74,13 @@ function addToCart(item) {
     if (existing) {
         existing.quantity += 1
     } else {
-        cart.push({ ...item })
+        cart.push({ ...item, quantity: item.quantity || 1 })
     }
 
     localStorage.setItem("cart", JSON.stringify(cart))
     updateCartCount()
-
-
-    alert("product added successfully");
+    cartPageUpdate()
 }
-
-
 
 
 
@@ -81,6 +88,17 @@ function cartPageUpdate() {
     productDetails.innerHTML = ""
 
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let price = []
+
+    if(!cart.length){
+        emptyContainer.innerHTML = ""
+        let div = document.createElement("div")
+        div.classList.add("empty-cart")
+        div.innerHTML = `<div >Your Cart is Empty</div>
+        <div>Looks like you haven’t added anything yet</div>`
+        emptyContainer.appendChild(div)
+ return;
+    }
 
     cart.forEach(item => {
         let tr = document.createElement("tr");
@@ -114,11 +132,21 @@ function cartPageUpdate() {
               </td>
             </tr>`
 
-        tr.querySelector(".actions").addEventListener('click', () => deleteItem(item))
+        tr.querySelector(".actions").addEventListener('click', () => {
+            deleteItem(item);
+            showToast("product removed" ,"error")
+        })
         tr.querySelector(".add").addEventListener('click', () => increasedQuantity(item))
         tr.querySelector(".min").addEventListener('click', () => decreasedQuantity(item))
+        // let amt =tr.querySelector(".prize").innerHTML
+        price.push(item.quantity * item.price)
+
         productDetails.appendChild(tr)
+
     })
+
+
+    displayOrderSummary(price)
     updateCartCount()
 
 }
@@ -152,9 +180,8 @@ function decreasedQuantity(item) {
     localStorage.setItem("cart", JSON.stringify(cart))
 
     cartPageUpdate()
-
-
 }
+
 function deleteItem(item) {
     let cart = JSON.parse(localStorage.getItem("cart"));
     cart = cart.filter(cart => item.id != cart.id);
@@ -163,10 +190,162 @@ function deleteItem(item) {
     cartPageUpdate()
 }
 
-
-
 function updateCartCount() {
     let cart = JSON.parse(localStorage.getItem("cart")) || []
     let count = cart.reduce((sum, item) => sum + item.quantity, 0);
     cartCount.textContent = count
+}
+
+let offers = [
+    { id: 1, code: "SAVE10", discount: 10, applied: false },
+    { id: 2, code: "SAVE20", discount: 20, applied: false },
+    { id: 3, code: "SAVE30", discount: 30, applied: false },
+    { id: 4, code: "SAVE50", discount: 50, applied: false }
+];
+
+
+function displayOrderSummary(price) {
+
+    let subTotalValue = price.reduce((sum, item) => sum + item, 0);
+    let diScountValue = subTotalValue * getAppliedDiscount()
+
+
+    let deliveryCharges = subTotalValue > 0 ? 50 : 0;
+    let totalValue = subTotalValue - diScountValue + deliveryCharges
+
+
+    subTotal.innerHTML = `${subTotalValue} USD`;
+    discount.innerHTML = `${diScountValue} USD`;
+    delivery.innerHTML = `${deliveryCharges} USD`;
+    total.innerHTML = `${totalValue} USD`
+}
+
+function getAppliedDiscount() {
+    let applied = offers.find(o => o.applied);
+    return applied ? applied.discount / 100 : 0;
+}
+
+function renderOfferList() {
+    availabeOffers.innerHTML = ""
+    offers.forEach((offer) => {
+        if (!offer.applied) {
+            let div = document.createElement("div");
+            div.classList.add("offer-item");
+            div.innerHTML = `<div >
+                             <span>SAVE${offer.discount} -</span>
+                            <span class="green">${offer.discount}% OFF</span>
+                        </div>
+                        <button class="btn">Apply</button>`;
+
+            div.querySelector(".btn").addEventListener("click", () => {
+                applyOffer(offer.id)
+                  
+            });
+
+            availabeOffers.appendChild(div);
+        }
+    })
+}
+
+renderOfferList()
+
+function applyOffer(id) {
+
+    offers.forEach(o => o.applied = false);
+
+    const selcted = offers.find(o => o.id == id);
+    if (selcted) {
+        selcted.applied = true;
+    }
+
+    renderOfferList()
+    renderAppliedOffer();
+
+    cartPageUpdate()
+
+}
+
+function renderAppliedOffer() {
+    appliedOfferList.innerHTML = "";
+
+    const applied = offers.find(o => o.applied);
+
+    if (applied) {
+        let div = document.createElement("div");
+        div.classList.add("applied-offer");
+
+        div.innerHTML = `
+              <div class="offer-text">
+                <i class="fa-solid fa-circle-check check-icon"></i>
+                <p>
+                  SAVE${applied.discount} applied <span class="green">(You saved$${applied.discount})</span>
+                </p>
+              </div>
+              <button class="remove">Remove</button>`;
+
+        div.querySelector(".remove").addEventListener("click", () => {
+            removeOffer(applied.id)
+            showToast("coupon removed" , "error")
+            
+        })
+
+        appliedOfferList.appendChild(div)
+        discountText.innerHTML = `${applied.discount}%`
+        showToast("coupon applied" ,"success")
+
+    } else {
+        discount.innerHTML = ""
+    }
+}
+
+
+function removeOffer(id) {
+    console.log(id)
+    const offer = offers.find(O => O.id == id);
+
+
+    if (offer) {
+        offer.applied = false;
+    }
+    renderOfferList();
+    renderAppliedOffer()
+
+    cartPageUpdate()
+    discountText.innerHTML = ""
+}
+
+
+applyBtn.addEventListener("click", () => {
+    let code = couponInput.value.trim().toUpperCase()
+
+    if (!code) {
+        alert("enter coupon code");
+        return;
+    }
+
+    let offer = offers.find(o=>o.code == code);
+
+    if(!offer){
+         showToast("Invalid coupon ", "error");
+        return;
+    }
+
+    applyOffer(offer.id)
+    couponInput.value =""
+})
+
+
+function showToast(message, type = "success") {
+    const container = document.getElementById("toast-container");
+
+    const toast = document.createElement("div");
+    toast.classList.add("toast", type);
+    toast.textContent = message;
+
+    container.appendChild(toast);
+
+    // remove after 3s
+    setTimeout(() => {
+        toast.remove();
+    }, 3000);
 }
