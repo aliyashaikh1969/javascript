ed565489e610d7f4f3e664eac1f8506b417c29fc
aliyashaikh1969 <aliyashaikh1969@gmail.com>
2026-07-11 16:45:29 +0530

    changing logic in booking project and api on chatbot project

--- projects/booking/app.js
+++ projects/booking/app.js
@@ -3,31 +3,84 @@ import { seats } from './data.js'
 const seatMap = document.querySelector(".seat-map")
 const input = document.querySelector(".input")
 const submit = document.querySelector("form button")
-    
+
 
 const TotalRow = 10
+let currentRow = 1
+let selectedSeat = [];
+let skippSeats = [];
 
-console.log(submit);
+
+const bookingSeat = (count) => {
+
+    let seatBook = count;
+
+    while (seatBook > 0 && currentRow <= TotalRow) {
+        seatBook = bookingSeatRow(seatBook)
+        currentRow++
+    }
+
+    while(seatBook>0&&skippSeats.length>0){
+        
+
+        // console.log(skippSeats,skippSeats.length);
+
+
+        let seat = skippSeats.shift();
+        seat.booked = true;
+        selectedSeat.push(seat);
+        seatBook--
+    }
+
+    renderUi()
+}
+
+
+const bookingSeatRow = (seatBook) => {
+
+    const rowsSeat = seats.filter(seat => seat.row === currentRow);
+    let bookingCount = Math.min(seatBook , rowsSeat.length)
+
+
+    for(let i =0 ; i<bookingCount ; i++){
+        rowsSeat[i].booked = true;
+        selectedSeat.push(rowsSeat[i])
+    }
+
+    for(let i =bookingCount ; i<rowsSeat.length ; i++){
+        skippSeats.push(rowsSeat[i]);
+    }    
+
+    return seatBook -= bookingCount
+
+}
 
 const renderUi = () => {
-    for (let i = 1; i < TotalRow; i++) {
-        let rowDiv = document.createElement("div");
+    seatMap.innerHTML = ""
+    for (let i = 0; i <= TotalRow; i++) {
+        const rowDiv = document.createElement("div");
         rowDiv.classList.add("row");
-        let seat = seats.filter(seat => seat.row == i)
-        for(let j =0 ; j<seat.length ; j++){
-            let div = document.createElement('div');
+        let seat = seats.filter(seat => seat.row === i)
+        seat.forEach(seat => {
+            const div = document.createElement('div');
             div.classList.add("col");
-            div.textContent = seat[j].id
+            div.textContent = seat.id
+            if (seat.booked) {
+                div.classList.add("booked")
+            }
             rowDiv.appendChild(div)
-        }
+        })
         seatMap.appendChild(rowDiv)
     }
 }
 renderUi()
 
-submit.addEventListener("click",(e)=>{
+submit.addEventListener("click", (e) => {
     e.preventDefault()
-    let value = input.value 
-    if(!value || value<0) return ;
+    let count = Number(input.value)
+    if (!count || count < 0) return;
+
+    bookingSeat(count)
     input.value = ""
+
 })
